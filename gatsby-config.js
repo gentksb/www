4d07d4ae1af1dd0806aa708e69b663223f0b1150
gensobunya <gen@gensobunya.net>
2020-01-21 14:55:35 +0900

    link datafield

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,63 +1,76 @@
 module.exports = {
   siteMetadata: {
-    title: '幻想サイクル Circle site & Portfolio',
-    description: '同人サイクル「幻想サイクル」公式WEBサイト兼、代表ゲンのポートフォリオ',
-    keywords: 'シクロクロス, 自転車同人誌, プログラミング, フロントエンド',
-    siteUrl: 'https://www.gensobunya.net/',
+    title: "幻想サイクル Circle site & Portfolio",
+    description:
+      "同人サイクル「幻想サイクル」公式WEBサイト兼、代表ゲンのポートフォリオ",
+    keywords: "シクロクロス, 自転車同人誌, プログラミング, フロントエンド",
+    siteUrl: "https://www.gensobunya.net/",
     author: {
-      name: 'gensobunya',
-      url: 'https://twitter.com/gen_sobunya',
-      email: 'gen@gensobunya.net'
-    }
+      name: "gensobunya",
+      url: "https://www.gensobunya.net/",
+      email: "gen@gensobunya.net",
+    },
+    social: {
+      twitter: `gen_sobunya`,
+      github: `gentksb`,
+      instagram: `gen_sobunya`,
+    },
   },
   plugins: [
-    'gatsby-plugin-lodash',
+    "gatsby-plugin-lodash",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'content',
-        path: `${__dirname}/content/portfolio`
-      }
+        name: "content",
+        path: `${__dirname}/content/portfolio`,
+      },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "linkdata",
+        path: `${__dirname}/content/data`,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 960
-            }
+              maxWidth: 960,
+            },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: 'margin-bottom: 1rem'
-            }
+              wrapperStyle: "margin-bottom: 1rem",
+            },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants'
-        ]
-      }
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
+        ],
+      },
     },
-    'gatsby-transformer-json',
+    "gatsby-transformer-json",
     {
-      resolve: 'gatsby-plugin-canonical-urls',
+      resolve: "gatsby-plugin-canonical-urls",
       options: {
-        siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
-      }
+        siteUrl: "https://gatsby-starter-typescript-plus.netlify.com",
+      },
     },
     {
-      resolve: 'gatsby-plugin-google-tagmanager',
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        id: 'YGTM-5R7P93N'
-      }
+        id: "YGTM-5R7P93N",
+      },
     },
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet'
-  ]
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+  ],
 }
