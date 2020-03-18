fc4db0d637631c4f2b870fd28e6ec341baf3e80a
gensobunya <gen@gensobunya.net>
2020-03-18 14:27:38 +0900

    add mui-theme

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,8 +2,10 @@ import React from "react"
 import Helmet from "react-helmet"
 import { StaticQuery, graphql } from "gatsby"
 import { Grid } from "@material-ui/core"
+import { ThemeProvider, StylesProvider } from "@material-ui/styles"
 
 import Sidebar from "../components/Sidebar"
+import theme from "./theme"
 
 interface StaticQueryProps {
   site: {
@@ -28,28 +30,33 @@ const IndexLayout: React.FC = ({ children }) => (
       }
     `}
     render={(data: StaticQueryProps) => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            {
-              name: "description",
-              content: data.site.siteMetadata.description,
-            },
-            { name: "keywords", content: data.site.siteMetadata.keywords },
-          ]}
-        >
-          <script type="text/javascript" src="https://cdn.iframe.ly/embed.js" />
-        </Helmet>
-        <Grid container spacing={0}>
-          <Grid item sm={4} xs="auto">
-            <Sidebar title={data.site.siteMetadata.title} />
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              {
+                name: "description",
+                content: data.site.siteMetadata.description,
+              },
+              { name: "keywords", content: data.site.siteMetadata.keywords },
+            ]}
+          >
+            <script
+              type="text/javascript"
+              src="https://cdn.iframe.ly/embed.js"
+            />
+          </Helmet>
+          <Grid container spacing={0}>
+            <Grid item sm={4} xs="auto">
+              <Sidebar title={data.site.siteMetadata.title} />
+            </Grid>
+            <Grid item sm={8} xs={12}>
+              {children}
+            </Grid>
           </Grid>
-          <Grid item sm={8} xs={12}>
-            {children}
-          </Grid>
-        </Grid>
-      </>
+        </ThemeProvider>
+      </StylesProvider>
     )}
   />
 )
---
+++ src/layouts/theme.tsx
@@ -0,0 +1,22 @@
+import { createMuiTheme } from "@material-ui/core"
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#e64946",
+      dark: "#B33836",
+      light: "#EA8E8C",
+    },
+    secondary: {
+      main: "#0F993B",
+      light: "#45E678",
+      dark: "#0A6627",
+    },
+    text: {
+      primary: "#262626",
+      secondary: "#C3D0D9",
+    },
+  },
+})
+
+export default theme
