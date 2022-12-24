074257467368610552b42a026775a40326ed89b1
gentksb <gen@gensobunya.net>
2022-12-24 21:51:06 +0900

    記事ページベース作成

--- src/components/Paper.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, ChakraProvider } from "@chakra-ui/react"
-
-export const Paper: React.FunctionComponent = ({ blog }) => {
-  return (
-    <ChakraProvider>
-      <Center>{blog}</Center>
-    </ChakraProvider>
-  )
-}
