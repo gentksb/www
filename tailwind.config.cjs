85bb8b4b857d8f181d0a51db58a034f600c34bbe
gentksb <gen@gensobunya.net>
2023-01-23 22:36:09 +0900

    カルーセル実装

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     container: {
       center: true
     },
     extend: {}
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")]
+  plugins: [
+    require("daisyui"),
+    require("@tailwindcss/typography"),
+    require("flowbite/plugin")
+  ]
 }
